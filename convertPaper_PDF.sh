d654b47216fd73d0498c66bc059ca6c408853efe
Jose Afonso Guerra-Assuncao <a.guerra@ucl.ac.uk>
2018-06-17 08:52:40 +0100

    Some changes to try this with kindle dxg too.

--- convertPaper_PDF.sh
+++ convertPaper_PDF.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-#EXTRA='-p 4- -n -ds 1.3' #selecing page numbers
-#EXTRA='-n -ds 1.3' #native pdf from djvu with scaling
+#EXTRA='-p 4-34 -n -ds 1.3' #selecing page numbers
+EXTRA='-n -ds 1.3' #native pdf from djvu with scaling
 #EXTRA='-n' #Native pdf
 #EXTRA='-ds 1.3' #Document scaling
 #EXTRA='-neg' #Negative output
-EXTRA='' #blank options
+#EXTRA='' #blank options
 
 ###Hey, please not that the margins change with the ds option above, so if a scaling factor is used, a scaled margin needs to be used too... (or so it seems)
 ###Margins in inches
 #large bottom margin
 #MARGINS='0,0,0,2'
 #Trim all around (default)
-MARGINS='0.3,0.7,0.7,0.4'
+#MARGINS='0.3,0.7,0.7,0.4'
+MARGINS='0.3,0.8,0.7,0.8' #For immune system book on -ds 1.3
+
 #Science mode
 #MARGINS='0.5,0.7,0.5,0.5'
 #Top only
@@ -24,33 +26,47 @@ MARGINS='0.3,0.7,0.7,0.4'
 #MARGINS='0,0.40,0,0.15' #with ds1.3
 #MARGINS='0,0.49,0,0.15'  #with ds1.3
 
-MAXCOL=4 #Maximum number of columns to be detected
-#MAXCOL=2 #Maximum number of columns to be detected
+#MAXCOL=4 #Maximum number of columns to be detected
+MAXCOL=2 #Maximum number of columns to be detected
 #MAXCOL=1 #Maximum number of columns to be detected
 
 #OCR options
 #OCRSTRING='' #default (faster)
-#OCRSTRING='-ocr t -ocrhmax 1 -ocrvis s -nt 2' #tesseract (needs separate installation, much slower, but more accurate)
+OCRSTRING='-ocr t -ocrhmax 1 -ocrvis s -nt 2' #tesseract (needs separate installation, much slower, but more accurate)
 
 #Filesize reduction
 #BWBIT=1 #BW
 #BWBIT=2 #4 Greys
-#BWBIT=4 #default (16 Greys)
+BWBIT=4 #default (16 Greys)
 
 
 #DEV="kpw" #Should be the right one but resolution not good enough, big margins
 DEV="kp2" #Seems to be perfect resolution for small margin mode
+DEV2=${DEV}
+
+
+#Confirmed no distortion on DXG, need to assess clipping
+#-w 788 -h 1133
+#-w 784 -h 1134
+#-w 783 -h 1135
+#-w 784 -h 1135
+#-w 786 -h 1136
+
+DEV="dx -w 786 -h 1136 "
+DEV2="dxg"
+
 
 #DITHERSTRING=''
-#DITHERSTRING='-d-' #No dithering
+DITHERSTRING='-d-' #No dithering
 
-#HYPHENS="-hy" #default
+HYPHENS="-hy" #default
 #HYPHENS="-hy-" #keep hyphens
 
 ######################## Finished setup, actually running stuff
 
 ###Mix of all the options above to make it as generic as possible and controlled by variables
-#k2pdfopt ${EXTRA} ${OCRSTRING} -m ${MARGINS} -bpc ${BWBIT} ${DITHERSTRING} -ehl 0 -evl 1 -ui- -dev ${DEV} -col ${MAXCOL} -cg 0.1 -cgmax 1 -ch 1 -cgr 0.33 -crgh 0.014 -comax 0.3 -o $1-${DEV}-BW.pdf -ow 20 -x -wrap ${HYPHENS} -ws -0.2 -j -1 -jf -1 0.75 -sm -cmax 1.5 -s- -g 0.5 -wt -1 $1
+k2pdfopt ${EXTRA} ${OCRSTRING} -m ${MARGINS} -bpc ${BWBIT} ${DITHERSTRING} -ehl 0 -evl 1 -ui- -dev ${DEV} -col ${MAXCOL} -cg 0.1 -cgmax 1 -ch 1 -cgr 0.33 -crgh 0.014 -comax 0.3 -o $1-${DEV2}-BW.pdf -ow 20 -x -wrap ${HYPHENS} -ws -0.2 -j -1 -jf -1 0.75 -sm -cmax 1.5 -s- -g 0.5 -wt -1 $1
+
 
 ###Native mode - book
 #k2pdfopt -mode fw -ui- -col ${MAXCOL} -dev ${DEV} $1
